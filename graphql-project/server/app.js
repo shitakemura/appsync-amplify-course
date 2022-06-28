8fd2f0319158a2ad155e7941bdf66c36f96b0a7c
shitakemura <shitakemura@gmail.com>
2022-06-28 22:17:36 +0900

    install graphql and express

--- graphql-project/server/app.js
+++ graphql-project/server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 
-app.listen(4000, () => {
+app.use('/graphql', graphqlHTTP({
+  graphiql: true
+}))
+
+app.listen(4000, () => { // localhost:4000
   console.log('Listening for requests on my awesome port 4000');
 })
