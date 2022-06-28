1fca0a5bfe1cbf244cb9efef6342dcd6a4a6fce4
shitakemura <shitakemura@gmail.com>
2022-06-28 23:00:18 +0900

    create RootQuery, run query on graphiql

--- graphql-project/server/app.js
+++ graphql-project/server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
+const schema = require('./schema/schema')
+
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  graphiql: true
+  graphiql: true,
+  schema,
 }))
 
 app.listen(4000, () => { // localhost:4000
--- graphql-project/server/schema/schema.js
+++ graphql-project/server/schema/schema.js
@@ -1,8 +1,8 @@
 const graphql = require('graphql');
 
 const {
+  GraphQLSchema,
   GraphQLObjectType, 
-  GraphQLID,
   GraphQLString,
   GraphQLInt
 } = graphql
@@ -12,9 +12,27 @@ const UserType = new GraphQLObjectType({
   name: 'User',
   description: 'Documentation for user...',
   fields: () => ({
-    id: {type: GraphQLID},
+    id: {type: GraphQLString},
     name: {type: GraphQLString},
     age: {type: GraphQLInt}
   })
 });
 
+// Root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  description: 'Description',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args) {
+        // get and return data from a datasource
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
