b3fa7435b1660eb28fb48b34ffc812a26df78dd3
shitakemura <shitakemura@gmail.com>
2022-06-29 22:11:20 +0900

    setup and connect to mongodb atlas

--- graphql-project/server/app.js
+++ graphql-project/server/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+const port = process.env.PORT || 4000
 
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
   schema,
 }))
 
-app.listen(4000, () => { // localhost:4000
-  console.log('Listening for requests on my awesome port 4000');
-})
+mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.fycgwjb.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
+ {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  app.listen({port : port}, () => { // localhost:4000
+    console.log(`Listening for requests on my awesome port ${port}`);
+  });
+ }).catch((error) => console.log(`Error: ${error}`))
+
