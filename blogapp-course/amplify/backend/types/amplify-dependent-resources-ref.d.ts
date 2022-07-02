8d46332aa17f05a2d3611568e081cb74859e01b7
shitakemura <shitakemura@gmail.com>
2022-07-03 00:02:42 +0900

    add auth with amplify cli

--- blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    blogappcourse: {
-      GraphQLAPIKeyOutput: 'string'
-      GraphQLAPIIdOutput: 'string'
-      GraphQLAPIEndpointOutput: 'string'
+    "api": {
+        "blogappcourse": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "blogappcourse5f23c5f6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
-  }
 }
--- blogapp-course/components/navbar.tsx
+++ blogapp-course/components/navbar.tsx
@@ -1,8 +1,31 @@
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Auth, Hub } from 'aws-amplify'
 
 export const Navbar = () => {
   const [signedUser, setSignedUser] = useState(false)
+
+  useEffect(() => {
+    authListener()
+  }, [])
+
+  const authListener = async () => {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return setSignedUser(true)
+        case 'signOut':
+          return setSignedUser(false)
+      }
+    })
+
+    try {
+      await Auth.currentAuthenticatedUser()
+      setSignedUser(true)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <nav className="flex justify-center space-x-4 border-b border-gray-300 bg-cyan-500 pt-3 pb-3">
@@ -19,6 +42,13 @@ export const Navbar = () => {
           </Link>
         )
       })}
+      {signedUser && (
+        <Link href={'/my-posts'}>
+          <a className="rounded-lg px-3 py-2 font-medium text-slate-700 hover:bg-slate-100 hover:text-slate-900">
+            My Post
+          </a>
+        </Link>
+      )}
     </nav>
   )
 }
