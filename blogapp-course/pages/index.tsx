752aacd77c3854b2da604f29fe22a4c1cb038e67
shitakemura <shitakemura@gmail.com>
2022-07-02 21:27:10 +0900

    add fetchPosts api request

--- blogapp-course/pages/index.tsx
+++ blogapp-course/pages/index.tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from 'next'
+import { API } from 'aws-amplify'
+import { listPosts } from '../src/graphql/queries'
+import { useEffect, useState } from 'react'
+import { Post, ListPostsQuery } from '../src/API'
+import { GraphQLResult } from '@aws-amplify/api-graphql'
 
 const Home: NextPage = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data } = (await API.graphql({
+        query: listPosts,
+      })) as GraphQLResult<ListPostsQuery>
+
+      const posts = [
+        ...((data?.listPosts?.items.filter((item) => item != null) as Post[]) ??
+          []),
+      ]
+      setPosts(posts)
+    }
+    fetchPosts()
+  }, [])
+
   return (
-    <h1 className="text-sky-400 text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="text-sky-400 text-3xl font-bold underline">MyPosts</h1>
+      {posts.map((post) => {
+        return <p key={post.id}>{post.content}</p>
+      })}
+    </div>
   )
 }
 
