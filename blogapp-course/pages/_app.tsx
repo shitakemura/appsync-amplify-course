ad3ab59d0150149e57d39d46557d9cdb1230f22e
shitakemura <shitakemura@gmail.com>
2022-07-02 21:47:30 +0900

    create NavBar component

--- blogapp-course/pages/_app.tsx
+++ blogapp-course/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../configureAmplify'
+import { Navbar } from '../components/navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <div className="py-8 px-16 bg-slate-100">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
 export default MyApp
