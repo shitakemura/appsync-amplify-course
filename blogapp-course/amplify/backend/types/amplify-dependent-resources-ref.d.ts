4f323233c64f990887e8102e7479cc866dcb26d2
shitakemura <shitakemura@gmail.com>
2022-07-02 12:32:34 +0900

    setup appsync api with amplify cli

--- blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ blogapp-course/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "blogappcourse": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
