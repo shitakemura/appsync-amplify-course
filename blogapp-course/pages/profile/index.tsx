40f090fe5a1b4b429adf778bf112e7880f612730
shitakemura <shitakemura@gmail.com>
2022-07-03 11:15:06 +0900

    add sign-out button

--- blogapp-course/pages/profile/index.tsx
+++ blogapp-course/pages/profile/index.tsx
@@ -9,10 +9,18 @@ const Profile = () => {
           <h1 className="mt-6 text-3xl font-semibold tracking-wide">
             Profile
           </h1>
-          <h1 className="my-2 font-medium text-gray-500">{user?.username}</h1>
+          <h1 className="my-2 font-medium text-gray-500">
+            UserName: {user?.username}
+          </h1>
           <p className="mb-6 text-sm text-gray-500">
-            {user?.attributes?.email}
+            Email: {user?.attributes?.email}
           </p>
+          <button
+            className="bg-orange-400 px-4 py-2 font-medium text-white"
+            onClick={signOut}
+          >
+            Sign out
+          </button>
         </div>
       )}
     </Authenticator>
