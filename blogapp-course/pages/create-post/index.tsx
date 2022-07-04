016a45f7e74464c9829f0c276b440059e90a2374
shitakemura <shitakemura@gmail.com>
2022-07-04 20:20:02 +0900

    update create-post page for file upload

--- blogapp-course/pages/create-post/index.tsx
+++ blogapp-course/pages/create-post/index.tsx
@@ -1,11 +1,11 @@
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { GraphQLResult } from '@aws-amplify/api-graphql'
-import { API } from 'aws-amplify'
+import { API, Storage } from 'aws-amplify'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { v4 as uuid } from 'uuid'
-import { CreatePostMutation } from '../../src/API'
+import { CreatePostInput, CreatePostMutation } from '../../src/API'
 import { createPost } from '../../src/graphql/mutations'
 import dynamic from 'next/dynamic'
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
@@ -17,18 +17,42 @@ const CreatePost = () => {
   const [post, setPost] = useState({ title: '', content: '' })
   const { title, content } = post
   const router = useRouter()
+  const [image, setImage] = useState<any>(null)
+  const imageFileInput = useRef<HTMLInputElement>(null)
 
   const createNewPost = async () => {
     if (!title || !content) return
     const id = uuid()
 
+    const postInput: CreatePostInput = {
+      id,
+      title,
+      content,
+    }
+
+    if (image) {
+      const filename = `${image.name}_${uuid()}`
+      postInput.coverImage = filename
+      await Storage.put(filename, image)
+    }
+
     const _ = (await API.graphql({
       query: createPost,
-      variables: { input: { ...post, id } },
+      variables: { input: postInput },
       authMode: 'AMAZON_COGNITO_USER_POOLS',
     })) as GraphQLResult<CreatePostMutation>
 
     router.push(`/posts/${id}`)
+  }
+
+  const uploadImage = async () => {
+    imageFileInput.current?.click()
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileUploaded = event.target.files ? event.target.files[0] : null
+    if (!fileUploaded) return
+    setImage(fileUploaded)
   }
 
   return (
@@ -55,6 +79,12 @@ const CreatePost = () => {
         onClick={createNewPost}
       >
         Create Post
+      </button>{' '}
+      <button
+        type="button"
+        className="mb-4 rounded-lg bg-purple-600 px-8 py-2 font-semibold text-white"
+      >
+        Upload Cover Image
       </button>
     </div>
   )
