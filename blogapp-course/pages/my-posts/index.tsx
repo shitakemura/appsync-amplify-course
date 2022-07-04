cf58b158e360aced4362698736390fc62154d7b4
shitakemura <shitakemura@gmail.com>
2022-07-04 09:00:18 +0900

    update style

--- blogapp-course/pages/my-posts/index.tsx
+++ blogapp-course/pages/my-posts/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { Post, PostsByUsernameQuery } from '../../src/API'
 import { postsByUsername } from '../../src/graphql/queries'
+import Moment from 'moment'
 
 const MyPosts: NextPage = () => {
   const [posts, setPosts] = useState<Post[]>([])
@@ -28,8 +29,8 @@ const MyPosts: NextPage = () => {
   }, [])
 
   return (
-    <div>
-      <h1 className="mt-6 mb-2 text-3xl font-semibold tracking-wide">
+    <div className="max-w-xxl mx-auto mb-2 rounded bg-white py-8 px-8 sm:items-center sm:space-y-0 sm:space-x-6">
+      <h1 className="mt-6 mb-8 text-3xl font-semibold tracking-wide">
         My Posts
       </h1>
       {posts.map((post) => (
@@ -37,6 +38,9 @@ const MyPosts: NextPage = () => {
           <div className="mt-8 cursor-pointer border-b border-gray-300 pb-4">
             <h2 className="text-xl font-semibold">{post.title}</h2>
             <p className="mt-2 text-gray-500">Author: {post.username}</p>
+            <p className="font-medium text-slate-500">
+              Created on: {Moment(post.createdAt).format('ddd, MMMM hh:mm a')}
+            </p>
           </div>
         </Link>
       ))}
