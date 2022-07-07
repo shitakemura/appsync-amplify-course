87d9139e39c5fca666197d7d0bd683c940a7fcbd
shitakemura <shitakemura@gmail.com>
2022-07-07 18:04:12 +0900

    add write a comment button

--- blogapp-course/pages/posts/[id].tsx
+++ blogapp-course/pages/posts/[id].tsx
@@ -12,11 +12,24 @@ import ReactMarkDown from 'react-markdown'
 import '../../configureAmplify'
 import { GetPostQuery, ListPostsQuery, Post } from '../../src/API'
 import { getPost, listPosts } from '../../src/graphql/queries'
+import dynamic from 'next/dynamic'
+import { Auth, Hub } from 'aws-amplify'
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+})
+import 'easymde/dist/easymde.min.css'
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 const Post: NextPage<Props> = ({ post }) => {
   const [coverImage, setCoverImage] = useState<string | null>(null)
+  const [comment, setComment] = useState({ message: '' })
+  const [showMe, setShowMe] = useState(false)
+
+  const toggle = () => {
+    setShowMe((prev) => !prev)
+  }
 
   useEffect(() => {
     const updateCoverImage = async () => {
@@ -53,6 +66,27 @@ const Post: NextPage<Props> = ({ post }) => {
       <p className="my-4 text-sm font-light">By {post.username}</p>
       <div className="mt-8">
         <ReactMarkDown className="prose">{post.content ?? ''}</ReactMarkDown>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          className="mb-4 rounded-lg bg-green-600 px-8 py-2 font-semibold text-white"
+          onClick={toggle}
+        >
+          Write a Comment
+        </button>
+        {
+          <div style={{ display: showMe ? 'block' : 'none' }}>
+            <SimpleMDE value="hello" />
+            <button
+              type="button"
+              className="mb-4 rounded-lg bg-green-600 px-8 py-2 font-semibold text-white"
+            >
+              Save
+            </button>
+          </div>
+        }
       </div>
     </div>
   )
